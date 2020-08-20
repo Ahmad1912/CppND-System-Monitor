93c8ae9a62cb3c83649ee00fe1367d3508c4e2cf
Ahmad Suliman <59208821+Ahmad1912@users.noreply.github.com>
2020-08-20 19:28:55 +0200

    Add files via upload

--- CppND-System-Monitor/include/linux_parser.h
+++ CppND-System-Monitor/include/linux_parser.h
@@ -44,7 +44,7 @@ std::vector<std::string> CpuUtilization();
 float CpuUtilization(int pid);
 long Jiffies();
 long ActiveJiffies();
-long ActiveJiffies(int pid);
+float ActiveJiffies(int pid);
 long IdleJiffies();
 
 // Processes
@@ -52,7 +52,7 @@ std::string Command(int pid);
 std::string Ram(int pid);
 std::string Uid(int pid);
 std::string User(int pid);
-long int UpTime(int pid);
+float UpTime(int pid);
 };  // namespace LinuxParser
 
 #endif
